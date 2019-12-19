bd1ca2a2602122955fc15ccb7b9a901638843995
Ihar Tsykala <Sudja09@bk.ru>
2019-12-19 23:37:51 +0300

    feat: add handler on search

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const closeMenu = document.getElementById("closeMenu")
 
 startMenu.addEventListener("click", () => (menu.style.display = "grid"))
 closeMenu.addEventListener("click", () => (menu.style.display = ""))
+
+const search = document.getElementById("search")
+const startSearch = document.getElementById("startSearch")
+const closeSearch = document.getElementById("closeSearch")
+
+startSearch.addEventListener("click", () => (search.style.display = "grid"))
+closeSearch.addEventListener("click", () => (search.style.display = ""))
